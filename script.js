e7387f3db3d2bbdba982a21452cda6b858468452
Olysar1 <giorgibarbakadz1@gmail.com>
2023-05-20 10:47:51 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//////////////////
+
+//Using the Geolocation API
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const location = `https://www.google.com/maps/@${latitude},${longitude}`;
+      console.log(location);
+    },
+    () => {
+      alert('could not get your position');
+    }
+  );
